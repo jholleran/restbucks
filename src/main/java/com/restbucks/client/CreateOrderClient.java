7f70531ccf344a3d9cde9b304116afeefa4a9162
Jarlath Holleran <jarlath.holleran@gmail.com>
2015-03-22 21:04:14 +0000

    added a java client to create an order

---
+++ src/main/java/com/restbucks/client/CreateOrderClient.java
@@ -0,0 +1,67 @@
+package com.restbucks.client;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+public class CreateOrderClient {
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		// HttpClient
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpPost post = new HttpPost(
+				"http://localhost:8080/restbucks/web/api/order");
+		post.setHeader("Content-Type", "application/xml");
+
+		String xml = "<ns2:order xmlns:ns2=\"http://schemas.restbucks.com/order\">"
+				+ "<items>"
+				+ "<item>"
+				+ "<milk>whole</milk>"
+				+ "<name>latte</name>"
+				+ "<quantity>1</quantity>"
+				+ "<size>small</size>"
+				+ "</item>"
+				+ "<item>"
+				+ "<kind>chocolate-chip</kind>"
+				+ "<name>cookie</name>"
+				+ "<quantity>2</quantity>"
+				+ "</item>"
+				+ "</items>"
+				+ "<location>takeaway</location>"
+				+ "<status>pending</status>"
+				+ "</ns2:order>";
+
+		HttpEntity entity = new ByteArrayEntity(xml.getBytes("UTF-8"));
+		post.setEntity(entity);
+		HttpResponse response = client.execute(post);
+
+		int responseCode = response.getStatusLine().getStatusCode();
+		System.out.println("response code: " + responseCode);
+		String result = EntityUtils.toString(response.getEntity());
+		System.out.println("response body: " + result);
+
+		if (responseCode == 201) {
+			System.out.println("Location: " + response.getHeaders("Location")[0].getValue());
+		} else if (responseCode == 400) {
+			// throw new BadRequestException();
+			System.out
+					.println("If we get a 400 response, the caller's gone wrong");
+		} else if (responseCode == 500 || responseCode == 503) {
+			// throw new
+			// ServerFailureException(post.getResponseHeader("Retry-After"));
+			System.out
+					.println("If we get a 5xx response, the caller may retry");
+		}
+
+		post.releaseConnection();
+	}
+
+}
